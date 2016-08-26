ff788bfab0f0cac2b03aaad5dcb2dbb4598af0cd
Jack Dunstan <jack@zxcv.net.au>
2016-08-26 14:28:31 +1000

    Made basic working forms

--- cultural-sensitivity-warning.php
+++ cultural-sensitivity-warning.php
@@ -24,9 +24,80 @@ function csw_plugin_menu() {
 /** Step 3. */
 function csw_plugin_options() {
 	if ( !current_user_can( 'manage_options' ) )  {
-		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+		wp_die( __( 'You do not have sufficient permissions to access this page. PLease ask an administrator for privilages ' ) );
+	}?>
+	<style>
+	input.csw_primary_text, 	input.csw_secondary_text
+	{
+
+width: 80%;
 	}
-	echo '<div class="wrap">';
-	echo '<p>Here is where the form would go if I actually had options.</p>';
-	echo '</div>';
+	</style>
+	<div class="wrap">
+	<h1>Popup Options</h1>
+	<?php $layout = get_option('theme_layout'); ?>
+	<form method="post" action="options.php">
+ 	        <?php
+ 	            settings_fields("section");
+ 	            do_settings_sections("post_type_options");
+ 	            submit_button();
+ 	        ?>
+ 	    </form>
+	<h2>Current Settings</h2>
+
+	<b>Style: </b><?php echo get_option('popup_style'); ?><br />
+	<b>Primary Text: </b><?php echo get_option('csw_primary_text'); ?><br />
+	<b>Secondary Text: </b><?php echo get_option('csw_secondary_text'); ?><br />
+	<b>Button Text: </b><?php echo get_option('csw_button_text'); ?><br />
+
+</div>
+<?php }
+
+function choose_style_type()
+{
+	?>
+<select name="popup_style">
+  <option value="basic" <?php checked(1, get_option('popup_style'), true); ?>>Basic</option>
+  <option value="bootstap" <?php checked(2, get_option('popup_style'), true); ?>>Bootstrap</option>
+  <option value="none" <?php checked(3, get_option('popup_style'), true); ?> >None</option>
+</select>
+<?php
 }
+function csw_primary_text_function()
+{
+	?>
+	<textarea type="textarea" class="csw_primary_text" name="csw_primary_text" name="" cols="80" rows="8" <?php checked(1, get_option('csw_primary_text'), true); ?>><?php echo get_option('csw_primary_text'); ?></textarea><br>
+	<?php
+}
+function csw_secondary_text_function()
+{
+	?>
+	<textarea type="textarea" class="csw_secondary_text" name="csw_secondary_text" name="" cols="80" rows="4" <?php checked(1, get_option('csw_secondary_text'), true); ?>><?php echo get_option('csw_secondary_text'); ?></textarea><br>
+
+	<?php
+}
+function csw_button_text_function()
+{
+	?>
+	<textarea type="textarea" class="csw_button_text" name="csw_button_text" name="" cols="40" rows="1" <?php checked(1, get_option('csw_button_text'), true); ?>><?php echo get_option('csw_button_text'); ?></textarea><br>
+
+	<?php
+}
+
+
+function display_theme_panel_fields()
+{
+	add_settings_section("section", "All Settings", null, "post_type_options");
+
+	add_settings_field("popup_style", "Select popup style", "choose_style_type", "post_type_options", "section");
+	add_settings_field("csw_primary_text", "Enter Primary Text", "csw_primary_text_function", "post_type_options", "section");
+	add_settings_field("csw_secondary_text", "Enter Secondary Text", "csw_secondary_text_function", "post_type_options", "section");
+	add_settings_field("csw_button_text", "Enter Button Text", "csw_button_text_function", "post_type_options", "section");
+
+	register_setting("section", "popup_style");
+	register_setting("section", "csw_primary_text");
+	register_setting("section", "csw_secondary_text");
+	register_setting("section", "csw_button_text");
+}
+
+add_action("admin_init", "display_theme_panel_fields");

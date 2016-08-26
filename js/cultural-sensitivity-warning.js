b23c1d0ebc8f0a0807980f0d71da6c0ba4374e4e
Luke <luke@zxcv.net.au>
2016-08-26 14:28:53 +1000

    dropped js, css and html in. Reworked to ditch jQuery and make universal

--- js/cultural-sensitivity-warning.js
+++ js/cultural-sensitivity-warning.js
@@ -1,44 +1,50 @@
 // Cultural Sensitivity Warning
 
+// All cookie-related functions live within the cookieControl object
+var cookieControl = {
+	function createCookie(name,value,days) {
+		if (days) {
+			var date = new Date();
+			date.setTime(date.getTime()+(days*24*60*60*1000));
+			var expires = "; expires="+date.toGMTString();
+		}
+		else var expires = "";
+		document.cookie = name+"="+value+expires+"; path=/";
+	},
 
-function createCookie(name,value,days) {
-	if (days) {
-		var date = new Date();
-		date.setTime(date.getTime()+(days*24*60*60*1000));
-		var expires = "; expires="+date.toGMTString();
+	function readCookie(name) {
+		var nameEQ = name + "=";
+		var ca = document.cookie.split(';');
+		for(var i=0;i < ca.length;i++) {
+			var c = ca[i];
+			while (c.charAt(0)==' ') c = c.substring(1,c.length);
+			if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+		}
+		return null;
+	},
+
+	function eraseCookie(name) {
+		createCookie(name,"",-1);
 	}
-	else var expires = "";
-	document.cookie = name+"="+value+expires+"; path=/";
 }
 
-function readCookie(name) {
-	var nameEQ = name + "=";
-	var ca = document.cookie.split(';');
-	for(var i=0;i < ca.length;i++) {
-		var c = ca[i];
-		while (c.charAt(0)==' ') c = c.substring(1,c.length);
-		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-	}
-	return null;
-}
+// iife to run function immediately without creating global variables.
+(function(){
+	var warned = cookieControl.readCookie('warned');
+	var sText = document.getElementById("csw-secondary-text");
+	var button = document.getElementById("csw-warning-button");
+	var csw = document.getElementById("csw-container");
 
-function eraseCookie(name) {
-	createCookie(name,"",-1);
-}
-
-$( document ).ready(function() {
-	var warned = readCookie('warned');
-
-	$('#warning-button').show();
-	$('.tr-cookie-desc').show();
+	sText.style.display = 'block';
+	button.style.display = 'block';
+	csw.className = 'csw-container-js';
 
 	if (warned === 'yes') {
-		$('#tr-content-warning').hide();
+		csw.style.display = 'none';
 	} else {
-		$('#tr-content-warning').addClass('tr-content-warning-background-js').removeClass('tr-content-warning-background');
-		$('#warning-button').on('click', function() {
-			createCookie('warned', 'yes', 7);
-			$('#tr-content-warning').hide();
+		button.onclick = function() {
+			cookieControl.createCookie('warned', 'yes', 7);
+			csw.style.display = 'none';
 		});
 	}
-});
+})();
---
+++ style-options/none.php
@@ -0,0 +1,15 @@
+<!-- none -->
+
+<div id="csw-container" class="csw-container">
+  <div class="csw-wrapper">
+    <p class="csw-primary-text">
+      <!-- insert csw text -->
+    </p>
+    <p id="csw-secondary-text" class="csw-secondary-text">
+      <!-- insert secondary csw text -->
+    </p>
+    <button id="csw-warning-button" class="csw-warning-button">
+      <!-- insert csw text -->
+    </button>
+  </div>
+</div>

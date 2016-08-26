f7c81dbec0e8efcb200b08aa6e754c5adbfb0b23
Luke <luke@zxcv.net.au>
2016-08-26 12:18:53 +1000

    setup new fikles

---
+++ cultural-sensitivity-warning.php
@@ -0,0 +1,15 @@
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+<?php
+/*
+Plugin Name: Cultural Sensitivity Warning
+Plugin URI:
+Description: A Wordpress plugin aimed at warning visitors of culturally sensitive material
+Version:     0.0.1
+Author:      Jack Dunstan and Luke Powell
+Author URI:
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Domain Path:
+Text Domain:
+*/

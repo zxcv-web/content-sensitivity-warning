52c42f58f15111b5a19329de3c1157c8bff957eb
Jack Dunstan <jack@zxcv.net.au>
2016-08-26 15:01:17 +1000

    Added bootstrap styles

--- style-options/bootstrap.php
+++ style-options/bootstrap.php
@@ -3,13 +3,13 @@
 <div id="csw-container" class="csw-container">
   <div class="csw-wrapper">
     <p class="csw-primary-text">
-      <!-- insert csw text -->
+      <?php echo get_option('csw_primary_text'); ?>
     </p>
     <p id="csw-secondary-text" class="csw-secondary-text">
-      <!-- insert secondary csw text -->
+      <?php echo get_option('csw_secondary_text'); ?>
     </p>
     <button id="csw-warning-button" class="csw-warning-button">
-      <!-- insert csw text -->
+      <?php echo get_option('csw_button_text'); ?>
     </button>
   </div>
 </div>
